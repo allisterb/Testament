1a83d3377fadf2b23f334427c5bc63b01b30055d
Allister Beharry <allister.beharry@gmail.com>
2017-10-21 19:29:59 -0400

    Use Testament.Api.Http name for Web Api project. Added Testament.Storage.Sql project with EFCore package.

--- Testament.Api.Http/Startup.cs
+++ Testament.Api.Http/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Testament.Api
+namespace Testament.Api.Http
 {
     public class Startup
     {
---
+++ Testament.Core/GrafeasService.cs
@@ -0,0 +1,9 @@
+﻿namespace Testament
+{
+    using System;
+    using Grafeas.V1Alpha1.Api;
+
+    public class GrafeasService : Grafeas.GrafeasBase
+    {
+    }
+}
